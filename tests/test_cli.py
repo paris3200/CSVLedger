983f52a8b974c7220bcfc03d4f4a46f1864f300f
Jason Paris <paris3200@gmail.com>
2019-12-04 14:14:27 -0500

    Add additional testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,3 +13,60 @@ def test_entrypoint():
     """
     exit_status = os.system("csvledger --help")
     assert exit_status == 0
+
+
+def test_sample_returns_unmatched_transaction():
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            "--config",
+            "tests/sample_config.json",
+            "-i",
+            "tests/sample_transactions.csv",
+            "--check",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "TARGET" in result.output
+
+
+def test_totals():
+    """ Test Totals on sample data """
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            "--config",
+            "tests/sample_config.json",
+            "-i",
+            "tests/sample_transactions.csv",
+            "--total",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "TARGET" not in result.output
+    assert "+$1200.00" in result.output
+    assert "-$653.70" in result.output
+
+
+def test_conversion():
+    """ Test coversion to ledger format """
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        [
+            "--config",
+            "tests/sample_config.json",
+            "-i",
+            "tests/sample_transactions.csv",
+            "--convert",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "2019/08/26 * PAY CHECK\n" in result.output
+    assert "\t \t Assets:Checking \t $1200.00" in result.output
+    assert "\t \t Income \n\n" in result.output
