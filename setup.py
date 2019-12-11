edfebc8e4a9753997a3d87bf67e4ebddc15ac7a5
Jason Paris <paris3200@gmail.com>
2019-12-11 09:04:29 -0500

    Add author information to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="csvledger",
     version="0.1",
+    author="Jason Paris",
+    author_email="paris3200@gmail.com",
+    description="convert csv to ledgercli format",
     py_modules=["csvledger"],
     install_requires=["Click"],
     entry_points="""
