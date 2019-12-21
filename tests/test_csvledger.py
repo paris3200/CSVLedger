fccd3fb86158b091be39b45e9aa359a2f756bdb9
Jason Paris <paris3200@gmail.com>
2019-12-20 20:00:41 -0500

    Fix bug when no filters in config
    Fix bug with no filters in config causing a Nonetype error.

--- tests/test_csvledger.py
+++ tests/test_csvledger.py
@@ -27,3 +27,10 @@ def test_format_debit_transaction_with_visa_profile():
     result = convertor.format_transaction("date", "BP", debit=20.00)
     formatted = "date * BP\n\t\tExpenses:Auto:Gas\t$20.00\n\t\tLiabilities:Visa\n\n"
     assert result == formatted
+
+
+def test_filter_description_no_filter():
+    convertor = CSVledger("tests/sample_config_no_filter.yml")
+    transaction = "ACH Debit BILLPAY TO DUKE ENERGY"
+    result = convertor.filter_description(transaction)
+    assert result == transaction
